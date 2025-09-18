bd5941dfe774ba4c4000f2023b0497175929b0b7
HannahSmirnova <alexamy1986@gmail.com>
2025-09-18 15:44:54 -0600

    footer added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Weather App</h1>
+    <div>
+      Weather App
+      <footer>
+        Coded by Anna Smirnova and is{" "}
+        <a
+          href="https://github.com/HannahSmirnova/new-weather-react-anna-smirnova.git"
+          target="_blank"
+        >
+          open-sourced
+        </a>
+      </footer>
     </div>
   );
 }

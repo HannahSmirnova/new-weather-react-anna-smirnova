f654661741f982fe24e1cdf9ffe52b89c9a452e3
HannahSmirnova <alexamy1986@gmail.com>
2025-09-19 22:16:54 -0600

    Weather converting units

--- src/SearchInfo.js
+++ src/SearchInfo.js
@@ -1,6 +1,21 @@
 import FormattedDate from "./FormattedDate";
+import { useState } from "react";
 
 export default function SearchInfo(props) {
+  const [unit, setUnit] = useState("celsius");
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  function fahrenheitTemp() {
+    return (props.info.temperature * 9) / 5 + 32;
+  }
+
   return (
     <div className="SearchInfo">
       <h1>{props.info.city}</h1>
@@ -13,13 +28,37 @@ export default function SearchInfo(props) {
       <div className="row">
         <div className="col-6">
           <img
+            className="pb-2"
             src={props.info.icon_url}
             height="80"
             width="80"
             alt={props.info.description}
           />
-          {Math.round(props.info.temperature)}°C
+          <span className="temperature-value">
+            {" "}
+            {unit === "celsius"
+              ? `${Math.round(props.info.temperature)}`
+              : `${Math.round(fahrenheitTemp())}`}
+          </span>
+          <span className="unit-toggle">
+            {unit === "celsius" ? (
+              <span className="active-unit">°C</span>
+            ) : (
+              <a href="/" onClick={showCelsius} className="inactive-unit">
+                °C
+              </a>
+            )}
+            {" | "}
+            {unit === "fahrenheit" ? (
+              <span className="active-unit">°F</span>
+            ) : (
+              <a href="/" onClick={showFahrenheit} className="inactive-unit">
+                °F
+              </a>
+            )}
+          </span>
         </div>
+
         <div className="col-6 second-column">
           <ul>
             <li>Humidity: {props.info.humidity}%</li>
